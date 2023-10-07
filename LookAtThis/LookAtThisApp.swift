52d58593f90478fe5f44f3c5dd695ae116241056
Yasin <yasincetin2014@gmail.com>
2023-10-07 13:42:41 +0300

    initial commit

---
+++ LookAtThis/LookAtThisApp.swift
@@ -0,0 +1,17 @@
+//
+//  LookAtThisApp.swift
+//  LookAtThis
+//
+//  Created by Yasin Cetin on 7.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct LookAtThisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
